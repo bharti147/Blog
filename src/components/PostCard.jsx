189ff6144f34939779aa21834da33e042177d5cf
Bharti <sahanibharti71@gmail.com>
2024-06-21 05:45:08 +0530

    postcard and select empty, notes done

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import service from '../appwrite/config'
-import {Link} from 'react-router-dom'
 
-function PostCard({$id, title, featuredImage}) {
-  return (
-    <Link to={`/post/${$id}`}>
-    <div className='w-full bg-gray-100 rounded-xl p-4'>
-      <div className='w-full justify-center mb-4'>
-        <img src={featuredImage}/>
-      </div>
-    </div>
-    </Link>
-  )
-}
+    useEffect(() => {
+        //TODO: make it more easy to understand
 
-export default PostCard
+        // if (authStatus ===true){
+        //     navigate("/")
+        // } else if (authStatus === false) {
+        //     navigate("/login")
+        // }
+        
+        //let authValue = authStatus === true ? true : false
+
+        if(authentication && authStatus !== authentication){
+            navigate("/login")
+        } else if(!authentication && authStatus !== authentication){
+            navigate("/")
+        }
+        setLoader(false)
+    }, [authStatus, navigate, authentication])
