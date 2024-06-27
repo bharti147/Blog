f6bc436631eb747143a3927cb3a5a8d30299f8f0
Bharti <sahanibharti71@gmail.com>
2024-06-27 18:58:52 +0530

    x'

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,72 +1,54 @@
 import React from 'react'
-import { Editor } from '@tinymce/tinymce-react'
-import { Controller } from 'react-hook-form'
+import {Editor } from '@tinymce/tinymce-react';
+import {Controller } from 'react-hook-form';
 
-function RTE({name, control, label, defaultValue = ""}) {
+
+export default function RTE({name, control, label, defaultValue =""}) {
   return (
-    <div>
+    <div className='w-full'> 
+    {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
 
-    {label && <label>{label}</label>}
-       <Controller
-          name ={name || "content"}
-          control = {control}
-          render = {({field:onChange})=>(
-            <Editor
-              initialValue= {defaultValue}
-              init = {{
-                initialValue: defaultValue,
-                height: 500,
-                menubar: true,
-                plugins: [],
-                toolbar: '',
-              }}
+    <Controller
+    name={name || "content"}
+    control={control}
+    render={({field: {onChange}}) => (
+        <Editor
+        initialValue={defaultValue}
+        init={{
+            initialValue: defaultValue,
+            height: 500,
+            menubar: true,
+            plugins: [
+                "image",
+                "advlist",
+                "autolink",
+                "lists",
+                "link",
+                "image",
+                "charmap",
+                "preview",
+                "anchor",
+                "searchreplace",
+                "visualblocks",
+                "code",
+                "fullscreen",
+                "insertdatetime",
+                "media",
+                "table",
+                "code",
+                "help",
+                "wordcount",
+                "anchor",
+            ],
+            toolbar:
+            "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
+            content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
+        }}
+        onEditorChange={onChange}
+        />
+    )}
+    />
 
-             onEditorChange={onChange}
-        
-            />
-          )}
-       />
-    </div>
+     </div>
   )
 }
-
-export default RTE
-/*
-
-Now, as our editor is designed as a separate component,
-& we'll use it somewhere eventually like post form.
-so , how wil we get reference from there.
-we can use forwardRef hook  (but we'll not use it here)
-we can also use Controller
-
-PASS ALL THE PARAMETERS NEEDED in RTE component.
-
-(control will be responsible for taking its states to main form)
-name - we will pass here ,the name in parameter or   
-       "content"
-control - we will pass here, the control in parameter. 
-         it will give the control to parent component, which will call it
-
-render - for rendering elements, firstly put a callback
-         then put all the values in it, which we will track
-         jse onChange field m kuch change ho to inform
-         
-         now in rendering brackets=> jo bhi element
-                      hme render krwana h like input field k liye input, editor k liye editor.
-                      Here, show editor with values u need
-                      (in onEditorChange value, pass our onChange)
-
-
-NOW, to use it , we will create a folder named postform , a new file PostForm.jsx in it.
-*/
-
-
-/*
-agr hme kisi form ka control chyie, is control ko pass krke rte m hm control le skte h uska,
-
-getvalues- to gab values of form
-
-in useform we can pass an object, in which we can values of our choice, (as key value pair) like:
- defaultValues
-
-*/
